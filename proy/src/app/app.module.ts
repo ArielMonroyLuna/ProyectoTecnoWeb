6cc1e7c16432638d7b972791a9d537c3906e95ca
ArielMonroyLuna <ariel.monroy@ucb.edu.bo>
2020-11-14 19:31:48 -0400

    '1er1

---
+++ proy/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PresentacionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
